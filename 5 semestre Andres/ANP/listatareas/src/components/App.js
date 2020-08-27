4c27234dba3cae42e9b432fbeb27e7a47748c596
Andres12309 <andrius12309@gmail.com>
2020-08-27 11:23:58 -0500

    Crear componente TodoList

--- 5 semestre Andres/ANP/listatareas/src/components/App.js
+++ 5 semestre Andres/ANP/listatareas/src/components/App.js
@@ -2,10 +2,14 @@ import React, {useState} from 'react';
 import logo from '../images/logo.svg';
 import '../styles/App.css';
 import UserList from "./UserList";
+import TodoList from "./TodoList";
 
 
-const App = (props) => {
-    return <UserList users={props.users}/>
-}
+const App = (props) => (
+    <>
+        <UserList users={props.users}/>
+        <TodoList/>
+    </>
+)
 
 export default App;
